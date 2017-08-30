d6509c7508852c80fe8a361b3936b4396a47098d
onlyep <250433177@qq.com>
2017-08-30 22:24:08 +0800

    项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  {{ ppg }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data (){
+    return {
+      ppg: 12
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
