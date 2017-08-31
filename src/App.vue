6ba72f9fa47b5cb74c6bd10e724c6265c356c12b
onlyep <250433177@qq.com>
2017-09-01 00:24:19 +0800

    加入goods,ratings,seller路由 修改components的相对路径

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      i am content
-    </div>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import header from './components/header/header'
+  import header from 'components/header/header'
   export default {
     components: {
       'v-header': header
@@ -22,5 +30,12 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    .tab-item
+      flex 1
+      text-align center
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      component: Seller
+    }
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
 })
