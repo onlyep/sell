ef35e32f78e7abacabe0411ccb28075263a7fa0f
onlyep <250433177@qq.com>
2017-09-03 13:28:31 +0800

    header完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,12 +30,32 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
+    <transition name="fade">
+    <div v-if="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
           <div class="star-wrapper">
             <star :size="48" :score="seller.score"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul class="supports" v-if="seller.supports">
+            <li class="support-item" v-for="item in seller.supports">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
           </div>
         </div>
       </div>
@@ -43,6 +63,7 @@
         <i class="icon-close" @click="hideDetail"></i>
       </div>
     </div>
+    </transition>
   </div>
 </template>
 
@@ -196,6 +217,11 @@ export default {
       height 100%
       overflow auto
       background rgba(7,17,27,.8)
+      backdrop-filter blur(10px)
+      &.fade-enter-active, &.fade-leave-active
+        transition: opacity .5s
+      &.fade-enter, &.fade-leave-to
+        opacity: 0
       .detail-wrapper
         width 100%
         min-height 100%
@@ -211,6 +237,63 @@ export default {
             margin-top 18px
             padding 2px 0
             text-align center
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px auto
+            .line
+              flex 1
+              position relative
+              top -7px
+              border-bottom 1px solid rgba(255,255,255,.2)
+            .text
+              padding 0 12px
+              font-size 14px
+              font-weight 700
+              color rgb(255,255,255)
+              line-height 14px
+          .supports
+            width 80%
+            margin 0 auto
+            .support-item
+              padding 0 12px
+              margin-bottom 12px
+              font-size 0
+              &:last-child
+                margin 0
+              .icon
+                display inline-block
+                vertical-align top
+                width 16px
+                height 16px
+                margin-right 6px
+                background-repeat no-repeat
+                background-size 16px 16px
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                font-size 12px
+                font-weight 200
+                color rgb(255,255,255)
+                line-height 16px
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding 0 12px
+              font-size 12px
+              font-weight 200
+              color rgb(255,255,255)    
+              line-height 24px
+              text-align justify
       .detail-close
         position relative
         width 32px
